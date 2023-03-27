76568952dd6e836b4607824dfbdf2c01054002a0
Chandanatc2004 <109573391+Chandanatc2004@users.noreply.github.com>
2023-03-27 09:43:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DataType
+{
+    class SByteExample
+    {
+        public static void Main(string[] args)
+        {
+            string name = "Bindu";
+            Console.WriteLine($"HelloWorld,this is {name}.");
+        }
+    }
+}
